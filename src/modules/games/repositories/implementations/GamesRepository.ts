21eb2b674edeb1991c6503a3a5014f45ebb7682e
fabiosvd <fabiosalvador85@yahoo.com.br>
2022-03-24 13:24:30 -0300

    challenge finished :heavy_check_mark:

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -20,8 +20,9 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query('.'); // Complete usando raw query
+    return this.repository.query('SELECT COUNT(id) FROM games');
   }
+  
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
       .createQueryBuilder()
